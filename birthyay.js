f21eded3b32df6b7eaa6eb1140db4d30adaeb5f5
Kelly Su <z5418860@ad.unsw.edu.au>
2023-06-15 18:42:12 +1000

    Created date excerise

---
+++ birthyay.js
@@ -0,0 +1,29 @@
+
+
+//                                      !!!! BITHYAY !!!! 
+
+
+
+// Firstly u need to enter the birthdays in a format in which getTime() is able to be used. 
+
+// Then, use the taught function formats to calculate, remember this may not be in the required unit of time ! 
+
+// Convert to days and then print out a sentence saying the difference in days 
+
+// NOTE: 
+//                      reminder on how to print with both strings and variables 
+//                      console.log("words u wanna write " + var_name + "other words.");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TO DO: 
+// ENTER UR CODE BELOW
+
+
+
+
+
+
+
+
+
