9e50031e4fc0e17b89b095bcf2741213aecfe7a3
Calley Chai <z5366974@student.unsw.edu.au>
2023-06-15 18:58:48 +1000

    feat: implemented starter file for function exercises

---
+++ calculator.js
@@ -0,0 +1,26 @@
+// Basic Calculator Functions
+
+// This function takes in two numbers and returns the sum
+function add(num1, num2) {
+    // TODO:
+}
+
+// This function takes in two numbers and returns 'a' raised to the power of 'b'
+function power(num1, num2) {
+    // TODO:
+}
+
+// This function takes in a number and prints out the the factors of the number
+function findFactors(num1) {
+    // TODO:
+}
+
+// Challenge: This function takes in a number and returns the reverse of the number
+// For example, reverse(1234) returns 4321
+// reverse(-563) returns -365
+function reverse(num1) {
+    // TODO:
+}
+
+// Test your code here with various inputs!
+console.log(add(1 + 42))    // should print 43
