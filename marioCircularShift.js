990488732d15755188b1b1983532dcb4e79b9d8f
Victoria Zhao <z5480947@ad.unsw.edu.au>
2023-06-29 19:06:16 +1000

    basic exercises template

---
+++ marioCircularShift.js
@@ -0,0 +1,23 @@
+// You have been given an ordered array. When your number of steps is positive, you should
+// shift everything to the right. 
+// For example, [Yoshi, Bowser, Toad, Luigi, Princess Daisy] shifted two steps to the right
+// should become [Luigi, Princess Daisy, Yoshi, Bowser, Toad]
+// HINT: think about the slides we just went through
+
+const characters = ['Yoshi', 'Bowser', 'Toad', 'Luigi', 'Mario'];
+let steps = 3; // your code must work for any positive value of 'steps' e.h. steps = 6 AND steps = 4
+console.log("the original array: ");
+console.log(characters);
+
+// WRITE YOUR CODE BELOW THIS LINE
+
+
+
+
+// WRITE YOUR CODE ABOVE THIS LINE
+
+console.log("the new, shifted array: ");
+console.log(characters);
+
+
+// TESTS, DO NOT TOUCH
---
+++ marioStrength.js
@@ -0,0 +1,21 @@
+/*Your first array exercise!!
+
+You have been given two arrays: one that corresponds to the names of each character, while the
+other array contains information about their RESPECTIVE strength levels. For example, Toad
+would directly correspond to a strength of 3. 
+
+Your task:
+    Find and print the characters with maximum and minimum strength
+    in the form:
+        Strongest: Princess Daisy, 9
+        Weakest: Yoshi, 7
+        Weakest: Mario, 7
+    (these are only PLACEHOLDER names and values, your own solution will differ)
+*/
+
+
+const characters = ['Mario', 'Princess Peach', 'Toad', 'Princess Daisy', 'Luigi', 'Bowser'];
+const strength = [6, 7, 3, 5, 7, 3];
+
+// TODO:
+// CODE BELOW
